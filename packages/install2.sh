04a63795620c311210095b6ba49c637219e481f0
stackv <vraj18052007@gmail.com>
2025-11-07 20:32:59 +0530

    Add files via upload

---
+++ packages/install2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Update system and install essential packages
+sudo pacman -Syu git base-devel --needed
+
+# Install firmware and graphics packages
+sudo pacman -S linux-firmware mesa vulkan-intel --needed
+
+# Install input and display packages
+sudo pacman -S libinput xf86-input-libinput seatd xorg-xwayland --needed
+
+# Add user to necessary groups
+sudo usermod -aG seat,input,video,audio $USER
+
+# Enable seatd service
+sudo systemctl enable --now seatd
+
+# Install ly display manager and enable service
+sudo pacman -S ly --needed
+sudo systemctl enable ly.service
+
+#extras
+sudo pacman -S firefox fastfetch
+
+# Reboot system at the end
+sudo reboot
